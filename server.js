c87fe02e896f139d3f1288ef4db74a5c5a8da6cb
Vladan <vladan.simic@gecko.rs>
2015-01-13 14:40:51 +0100

    proc file added

--- server.js
+++ server.js
@@ -18,8 +18,9 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));                                         // log every request to the console
 mongoose.connect('mongodb://admin:admin@ds031561.mongolab.com:31561/telemedicina');
 //mongoose.connect("mongodb://localhost/telemedicina");
+app.set('port', (process.env.PORT || 5000));
 
-var port = /*process.env.PORT || */3000;        // set our port
+//var port = /*process.env.PORT || */3000;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
